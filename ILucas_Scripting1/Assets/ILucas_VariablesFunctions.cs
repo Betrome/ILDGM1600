f23160429fa2d794ae8c22b41ee4ac19c18395f3
Ian Lucas <spiratsu101@gmail.com>
2015-09-01 09:12:50 -0600

    Pushing the entire project. Also completed Variables and Functions.

---
+++ ILucas_Scripting1/Assets/ILucas_VariablesFunctions.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ILucas_VariablesFunctions : MonoBehaviour
+{
+
+	int ex = 12;
+
+	void Start()
+	{
+
+		ex = Multiply (ex);
+		Debug.Log (ex);
+
+	}
+	
+	// Update is called once per frame
+	int Multiply (int number)
+	{
+		int ret;
+		ret = number * 2;
+		return ret;
+	}
+}
