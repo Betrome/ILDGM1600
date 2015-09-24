d5aecb059148a54266861b8e40011d89aa748edb
Ian Lucas <spiratsu101@gmail.com>
2015-09-24 09:45:35 -0600

    completed the for script. Need to do while.

--- ILucas_Scripting1/Assets/IL_Loop.cs
+++ ILucas_Scripting1/Assets/IL_Loop.cs
@@ -6,12 +6,12 @@ public class IL_Loop : MonoBehaviour {
     public int maxHp = 0;
 
 	// Use this for initialization
-	void Start () {
+	/*void Start () {
 	
-	}
+	}*/
 	
 	// Update is called once per frame
-	void Update () {
+	void Start () {
         for (int hp = 3;  hp <= 100; hp++)
         {
             Debug.Log("HP: " + hp);
