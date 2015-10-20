cdf1a31b6df0e25b666f11e3be91f24c0653b56d
Ian Lucas <spiratsu101@gmail.com>
2015-10-20 09:43:23 -0600

    made the Array script.

---
+++ ILucas_Scripting1/Assets/IL_Array.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IL_Array : MonoBehaviour {
+
+    public GameObject[] ball;
+
+    void Start()
+    {
+        ball = GameObject.FindGameObjectsWithTag("Ball");
+
+        for (int i = 0; i < ball.Length; i++)
+        {
+            Debug.Log("Ball Number " + i + " is " + ball[i].name);
+        }
+    }
+}
