8be1d1b8eaa29d54b324a00d6d23d40974dbcdb7
Ian Lucas <spiratsu101@gmail.com>
2015-10-29 09:20:58 -0600

    completed the enumerator script.

---
+++ ILucas_Scripting1/Assets/IL_Enumeration.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IL_Enumeration : MonoBehaviour {
+
+    enum compass { North, East, South, West };
+
+    void Start()
+    {
+        compass Direction;
+        compass dir;
+
+        Direction = compass.North;
+
+        ChangeDirection (Direction);
+
+    }
+
+        compass ChangeDirection(compass dir)
+    {
+            if (dir == compass.North)
+                dir = compass.South;
+            else if (dir == compass.South)
+                dir = compass.North;
+            else if (dir == compass.East)
+                dir = compass.West;
+            else if (dir == compass.West)
+                dir = compass.East;
+
+            Debug.Log("the direction is " + dir);
+            return dir;
+    }  
+}
